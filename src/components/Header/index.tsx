2ea986ebccc898a51fd1faf773de56b0b027fb9d
Vitor Monteiro <mluizvitor@gmail.com>
2022-05-08 00:49:31 -0300

    🚧 Initial theme toggler

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,28 +1,43 @@
 export function Header() {
+
+  function darkActivator() {
+    const appRoot = document.documentElement;
+
+    const hasDarkClass = appRoot.classList.contains('dark');
+
+    if (hasDarkClass) {
+      appRoot.classList.remove('dark');
+      return null;
+    }
+
+    appRoot.classList.add('dark');
+    return null;
+  }
+
   return (
-    <header className='bg-zinc-900 h-[72px] px-6 py-3 flex fixed w-full justify-center gap-8j'>
-      <div className='max-w-[1120px] flex flex-1 items-center justify-between'>
-        <div className='h-9 w-full max-w-[160px] bg-zinc-800 rounded-lg' />
+    <header className='bg-light-surface-primary dark:bg-dark-surface-secondary shadow-md h-[72px] px-6 py-3 flex fixed w-full justify-center gap-8 z-[16]'>
+      <div className='max-w-[1120px] flex flex-1 items-center justify-between gap-8'>
+        <div className='h-9 w-full max-w-[160px] bg-light-surface-secondary-hover dark:bg-dark-surface-secondary-hover rounded-lg' />
         
         <nav className='hidden sm:flex flex-1 justify-center gap-8'>
           {
-            [...Array(4)].map(() => (
-              <>
-                <div className='h-4 min-w-[40px] w-full max-w-[100px] rounded-lg bg-zinc-800' />
-              </>
+            [...Array(4)].map((e, i) => (
+              <div key={i}
+                className='h-4 min-w-[40px] w-full max-w-[100px] rounded-lg bg-light-surface-secondary-hover dark:bg-dark-surface-secondary-hover' />
             ))
           }
         </nav>
 
         <div className='flex items-center'>
           {
-            [...Array(2)].map(() => (
-              <>
-                <div className='bg-zinc-800 rounded-lg h-8 w-8 mr-4'/>
-              </>
+            [...Array(2)].map((e,i) => (
+              <div key={i}
+                className='rounded-lg h-8 w-8 mr-4 bg-light-surface-secondary-hover dark:bg-dark-surface-secondary-hover'/>
             ))
           }
-          <div className='bg-zinc-800 rounded-[24px] h-12 w-12'/>
+          
+          <div onClick={darkActivator}
+            className='rounded-[24px] h-12 w-12 bg-light-surface-secondary-hover dark:bg-dark-surface-secondary-hover'/>
         </div>
       </div>
     </header>
